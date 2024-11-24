078cadb5d5fa0c95f595494b650454d0ce5717f1
Milos <dev980lop@gmail.com>
2024-11-24 13:43:01 +0100

    Login page displays successfully, but CORS  blocking the call to the backend.

--- frontend/app/src/login/login.component.ts
+++ frontend/app/src/login/login.component.ts
@@ -1,26 +1,28 @@
 import { Component, OnInit  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HttpClient} from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'login-component',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrls: ['../assets/static/vendors/mdi/css/materialdesignicons.min.css',
     '../assets/static/vendors/css/vendor.bundle.base.css',
     '../assets/static/css/style.css', '../assets/static/css/my.css']
 })
 
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit
+{
 
   ngOnInit() {
-    this.loadScript('../assets/static/js/off-canvas.js');
+  /*  this.loadScript('../assets/static/js/off-canvas.js');
     this.loadScript('../assets/static/js/hoverable-collapse.js');
     this.loadScript('../assets/static/js/misc.js');
     this.loadScript('../assets/static/js/settings.js');
-    this.loadScript('../assets/static/js/todolist.js');
+    this.loadScript('../assets/static/js/todolist.js');*/
   }
 
   private loadScript(src: string) {
@@ -35,19 +37,18 @@ export class LoginComponent implements OnInit {
     password: ''
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+
+  }
+
+  private postRequest(url: string) {
+    this.http.post(url, this.credentials, {responseType: 'json'}).subscribe(buffer => {
+      alert(buffer);
+    });}
 
   onSubmit() {
-    const url = 'https://your-backend-api.com/login'; // Replace with your API endpoint
-    this.http.post(url, this.credentials).subscribe(
-      response => {
-        alert('Login successful:');
-        // Handle success (e.g., store tokens, redirect)
-      },
-      error => {
-        alert('Login failed:');
-        // Handle error (e.g., show error message)
-      }
-    );
-  }
+    const url = "http://localhost:8080/password";
+    this.postRequest(url);
+    }
+
 }
